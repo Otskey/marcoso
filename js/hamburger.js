6f9893d0aa01f7c0f3c064ba270339a8aa46ef08
Scott Godfrey <Scott.godfrey@wedo.global>
2024-02-22 13:40:33 +0000

    updated hamburger menu and functionality.

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,12 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let hamburgerIcon = document.querySelector('.material-symbols-outlined');
+  let hamburgerIcon = document.querySelector('nav .material-symbols-outlined:first-of-type');
+  let closeIcon = document.querySelector('nav .material-symbols-outlined:nth-of-type(2)');
   let menu = document.querySelector('nav ul');
 
-  hamburgerIcon.addEventListener('click', function() {
-    // Log to the console when the hamburger icon is clicked
-    console.log("Hamburger icon clicked");
-
+  // Function to toggle the menu open and closed
+  function toggleMenu() {
     menu.classList.toggle('show');
-    hamburgerIcon.classList.toggle('active');
+    hamburgerIcon.style.display = menu.classList.contains('show') ? 'none' : 'block'; // Toggle hamburger icon visibility
+    closeIcon.style.display = menu.classList.contains('show') ? 'block' : 'none'; // Toggle close icon visibility
+  }
+
+  // Event listener for hamburger icon click
+  hamburgerIcon.addEventListener('click', toggleMenu);
+
+  // Event listener for close icon click
+  closeIcon.addEventListener('click', toggleMenu);
+
+  // Event listener for scroll
+  window.addEventListener('scroll', function() {
+    // Close the menu if it's open and the user scrolls
+    if (menu.classList.contains('show')) {
+      toggleMenu(); // Close the menu
+    }
   });
 });
