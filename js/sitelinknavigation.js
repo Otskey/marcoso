32936a7ca84070d4ff5d1296f5094191dda4452c
Scott Godfrey <Scott.godfrey@wedo.global>
2024-02-26 15:57:49 +0000

    refactored code/cleaned unused code
    amended java script to complete smooth scrolling
    
    added anchor to logo for smooth scrolling back to top.

---
+++ js/sitelinknavigation.js
@@ -0,0 +1,49 @@
+// JavaScript to scroll to sections when a link is clicked
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+
+    if (target) {
+      // Check if the target is the contact us section
+      if (this.getAttribute('href') === '#services') {
+        document.querySelector('.services-content').scrollIntoView({
+          behavior: 'smooth',
+          block: 'start' // Adjust this value if needed
+        });
+      }
+      else if (this.getAttribute('href') === '#products') {
+        document.querySelector('.steel-products').scrollIntoView({
+          behavior: 'smooth',
+          block: 'start' // Adjust this value if needed
+        });
+      }
+      else if (this.getAttribute('href') === '#contact-us') {
+        document.querySelector('.contact-us-content').scrollIntoView({
+          behavior: 'smooth',
+          block: 'start' // Adjust this value if needed
+        });
+      }
+      // Check if the target is the our locations section
+      else if (this.getAttribute('href') === '#our-locations') {
+        document.querySelector('.our-locations').scrollIntoView({
+          behavior: 'smooth',
+          block: 'start' // Adjust this value if needed
+        });
+      }
+      else if (this.getAttribute('href') === '#hero') {
+        document.querySelector('.hero').scrollIntoView({
+          behavior: 'smooth',
+          block: 'start' // Adjust this value if needed
+        });
+      }
+      else {
+        target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start' // Adjust this value if needed
+        });
+      }
+    }
+  });
+});
